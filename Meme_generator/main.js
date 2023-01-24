2d4e6ae197bf15b73428b0231a7c26c4c0971a95
AkshayaAgarwal <117337745+AkshayaAgarwal@users.noreply.github.com>
2023-01-24 12:21:53 +0530

    Add files via upload

---
+++ Meme_generator/main.js
@@ -0,0 +1,21 @@
+const generatememebtn = document.querySelector(".meme-generator .generate-meme-btn");
+const memeImage = document.querySelector(".meme-generator img");
+const memeTitle = document.querySelector(" .meme-generator .meme-title");
+const memeAuthor= document.querySelector(" .meme-generator .meme-author");
+
+const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML=title;
+    memeAuthor.innerHTML= `MEME by: ${author}`;
+
+};
+const generateMeme=() => {
+    fetch("https://meme-api.herokuapp.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then((data) => {
+        updateDetails(data.url, data.title, data.author);
+    });
+};
+
+generatememebtn. addEventListener("click", generateMeme);
+generateMeme();
